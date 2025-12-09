89a19303a8e15cd4d6531a7dc2c76165590068ba
PapazScripter <pedrootavio123ti@gmail.com>
2025-12-09 17:25:06 -0300

    Update gameLogic.ts

--- utils/gameLogic.ts
+++ utils/gameLogic.ts
@@ -1,1 +1,20 @@
+// utils/gameLogic.ts
+
+export function startGame() {
+  console.log("startGame() foi chamado");
+}
+
+export function stopGame() {
+  console.log("stopGame() foi chamado");
+}
+
+export function getRandomEvent() {
+  return "Evento aleatório gerado!";
+}
+
+export default {
+  startGame,
+  stopGame,
+  getRandomEvent
+};
 
