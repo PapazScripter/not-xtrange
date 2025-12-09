6c3a69a017062cfad94836c01706b999573df37a
PapazScripter <pedrootavio123ti@gmail.com>
2025-12-09 17:30:38 -0300

    Update audio.ts

--- utils/audio.ts
+++ utils/audio.ts
@@ -1,1 +1,15 @@
+// utils/audio.ts
+
+export function setupAudio() {
+  console.log("Audio system initialized.");
+}
+
+export function playSound(name: string) {
+  console.log("Playing sound:", name);
+}
+
+export default {
+  setupAudio,
+  playSound
+};
 
